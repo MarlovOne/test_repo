d9ed8fe155c83b977b469377b139309e69115f73
Lovro Markovic <lovro@janeasystems.com>
2025-01-16 19:20:06 +0100

    remove spdlog

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -6,18 +6,6 @@ include(cmake/CPM.cmake)
 function(test_repo_setup_dependencies)
   # For each dependency, see if it's
   # already been provided to us by a parent project
-  if(NOT TARGET spdlog::spdlog)
-    cpmaddpackage(
-      NAME
-      spdlog
-      VERSION
-      1.11.0
-      GITHUB_REPOSITORY
-      "gabime/spdlog"
-      OPTIONS
-      "SPDLOG_FMT_EXTERNAL ON")
-  endif()
-
   if(NOT TARGET Catch2::Catch2WithMain)
     cpmaddpackage("gh:catchorg/Catch2@3.3.2")
   endif()
