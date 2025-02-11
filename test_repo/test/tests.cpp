ddc37180ced15c71dfebbd334a3539876423a25c
Lovro Markovic <lovro@janeasystems.com>
2025-02-11 20:48:59 +0100

    fixes

--- test_repo/test/tests.cpp
+++ test_repo/test/tests.cpp
@@ -6,9 +6,9 @@
 
 TEST_CASE("Factorials are computed", "[factorial]")
 {
-  REQUIRE(factorial(0) == 1);
-  REQUIRE(factorial(1) == 1);
-  REQUIRE(factorial(2) == 2);
-  REQUIRE(factorial(3) == 6);
-  REQUIRE(factorial(10) == 3628800);
+  REQUIRE(test_repo::factorial(0) == 1);
+  REQUIRE(test_repo::factorial(1) == 1);
+  REQUIRE(test_repo::factorial(2) == 2);
+  REQUIRE(test_repo::factorial(3) == 6);
+  REQUIRE(test_repo::factorial(10) == 3628800);
 }
