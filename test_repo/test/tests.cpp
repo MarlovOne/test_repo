05b25bb817c59f72bce3a26be31cacb0291ef2ca
Lovro Markovic <lovro@janeasystems.com>
2025-03-04 23:37:58 +0100

    add opencv to tests

--- test_repo/test/tests.cpp
+++ test_repo/test/tests.cpp
@@ -1,11 +1,13 @@
 #include <catch2/catch_test_macros.hpp>
 
 
+#include <opencv2/opencv.hpp>
 #include <test_repo/sample_library.hpp>
 
 
 TEST_CASE("Factorials are computed", "[factorial]")
 {
+  cv::VideoCapture cap(0);
   REQUIRE(test_repo::factorial(0) == 1);
   REQUIRE(test_repo::factorial(1) == 1);
   REQUIRE(test_repo::factorial(2) == 2);
