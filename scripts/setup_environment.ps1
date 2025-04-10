1dc8d0a7b309e08715712eabb1edef0113cd6669
Lovro Markovic <lovro@janeasystems.com>
2025-04-10 21:41:01 +0200

    Update environment setup

--- scripts/setup_environment.ps1
+++ scripts/setup_environment.ps1
@@ -2,5 +2,6 @@
 
 # Directly set Git to rewrite the URL for the specific repository
 git config --global url."git@github.com:MarlovOne/flir-sdk".insteadOf "https://github.com/MarlovOne/flir-sdk"
+git config --global url."git@github.com:MarlovOne/sla-sdk".insteadOf "https://github.com/MarlovOne/sla-sdk"
 
 Write-Output "Git URL rewriting has been configured successfully."
